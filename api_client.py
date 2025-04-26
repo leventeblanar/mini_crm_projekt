0965967d23e5b69d726d2766f0f41d9ab834ac64
leventeblanar <blanar.levente@gmail.com>
2025-04-27 00:37:01 +0200

    api connection established

--- api_client.py
+++ api_client.py
@@ -1,1 +1,21 @@
+import requests
+
+header = "x-api-key: reqres-free-v1"
+
+class APIclient:
+    BASE_URL = "https://reqres.in/api"
+    API_KEY = "reqres-free-v1"
+    
+    def __init__(self):
+        self.headers = {
+            "x-api-key": self.API_KEY
+        }
+        
+    def get_users(self, page=1):
+        url = f"{self.BASE_URL}/users?page={page}"
+        response = requests.get(url, headers=self.headers)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception (f"Hiba történt a userek lekérdezése során.")
 
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+from api_client import APIclient
 
+def main():
+    client = APIclient()
+    
+    try:
+        users_data = client.get_users()
+        print("A felhasználók listája:")
+        for user in users_data["data"]:
+            print(f"{user['id']}: {user['first_name']} {user['last_name']} - {user['email']}")
+    except Exception as e:
+        print(str(e))
+    
+
+if __name__ == '__main__':
+    main()
