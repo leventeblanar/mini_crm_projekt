8840bf320fc0e431b742f4ed4dcbaebfb7f42bc1
leventeblanar <blanar.levente@gmail.com>
2025-04-27 16:32:12 +0200

    new user creation function

--- api_client.py
+++ api_client.py
@@ -1,4 +1,9 @@
 import requests
+import sys
+
+sys.stdout.reconfigure(encoding='utf-8')
+sys.stderr.reconfigure(encoding='utf-8')
+
 
 header = "x-api-key: reqres-free-v1"
 
@@ -17,5 +22,18 @@ class APIclient:
         if response.status_code == 200:
             return response.json()
         else:
-            raise Exception (f"Hiba történt a userek lekérdezése során.")
+            raise Exception (f"Error while requesting user data.")
 
+    def create_user(self, first_name, last_name, email, avatar):
+        url = f"{self.BASE_URL}/users"
+        new_user_data = {
+            'first_name': first_name,
+            'last_name':last_name,
+            'email': email,
+            'avatar': avatar
+            }
+        response = requests.post(url, headers=self.headers, json=new_user_data)
+        if response.status_code == 200 or 201:
+            return response.json()
+        else:
+            raise Exception ("Error while uploading new user.")
--- main.py
+++ main.py
@@ -4,16 +4,48 @@ def main():
     client = APIclient()
     
     try:
+        # GET user data
         users_data = client.get_users()
-        print("A felhasználók listája:")
+        print("List of already existing users:")
         for user in users_data["data"]:
             print(f"UserID: {user['id']}")
             print(f"First name: {user['first_name']}")
             print(f"Last name: {user['last_name']}")
             print(f"email: {user['email']}")
             print(f"-"*40)
+            
+        # POST new user
+        print("Create new user...")
+
+        first_name = str(input("First name: ").capitalize())
+        last_name = str(input("Last name: ").capitalize())
+        
+        email_valid = False
+
+        while not email_valid:
+            email = str(input("Email: "))
+            if "@" not in email or "." not in email:
+                print("Please provide a valid email address.")
+            else:
+                email_valid = True
+            
+        avatar_valid = False
+            
+        while not avatar_valid:
+            avatar = str(input("Avatar link: "))
+            if "https://" not in avatar:
+                print("Please provide a valid link for the avatar.")
+            else:
+                avatar_valid = True
+                
+        new_user = client.create_user(first_name, last_name, email, avatar)
+        print("New user creation is successfull.")
+        print(new_user)
+            
+            
     except Exception as e:
         print(str(e))
+    
     
 
 if __name__ == '__main__':
